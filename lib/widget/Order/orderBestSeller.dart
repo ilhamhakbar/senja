6a7bae8ac6f20a3cfdbfa0c7a8ffbd0dc8898389
Ilham Akbar <ilhamhakbar@gmail.com>
2019-11-03 13:47:22 +0700

    Order page

--- lib/widget/Order/orderBestSeller.dart
+++ lib/widget/Order/orderBestSeller.dart
@@ -61,7 +61,20 @@ class _OrderBestSellerState extends State<OrderBestSeller> {
             alignment: Alignment.centerLeft,
             child: Text(
               "Coffee",
-              style: h4,
+              style: h3,
+            ),
+          ),
+          Container(
+            // padding: EdgeInsets.only(top: 10.0),
+            margin: EdgeInsets.only(left: 15.0),
+            child: Column(
+              children: <Widget>[
+                (foodMenu == null || isLoading)
+                    ? Container(
+                        child: CircularProgressIndicator(),
+                      )
+                    : foodCardCategoryContainer(),
+              ],
             ),
           ),
         ],
@@ -95,17 +108,62 @@ class _OrderBestSellerState extends State<OrderBestSeller> {
                       // padding: EdgeInsets.only(left: 10.0),
                       alignment: Alignment.topLeft,
                       child: Text(foodCategory.title,
-                          style: TextStyle(color: Colors.black))),
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold))),
                   Container(
-                    // padding: EdgeInsets.only(left: 10.0),
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      "Espresso + Milk + Water + Ice",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                  Container(
+                      padding: EdgeInsets.only(top: 10.0),
                       // height: 100,
                       // width: 100,
                       // padding: EdgeInsets.only(left: 5.0, right: 5.0),
                       // alignment: Alignment.bottomCenter,
                       child: Text(
-                    foodCategory.numbers.toString(),
-                    style: TextStyle(color: Colors.black),
-                  ))
+                        foodCategory.numbers.toString(),
+                        style: TextStyle(color: Colors.black),
+                      ))
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 30,
+      width: 30,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[ FloatingActionButton(
+          backgroundColor: Colors.deepOrange,
+          foregroundColor: Colors.indigo,
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+      ]),
+                        ),
+                        SizedBox(
+                          width: 30.0,
+                          height: 30.0,
+                          child: FloatingActionButton(
+                            backgroundColor: Color(0xff02c39a),
+                            onPressed: () {},
+                            child: Icon(Icons.add),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container()
                 ],
               ),
             ),
@@ -122,6 +180,7 @@ class _OrderBestSellerState extends State<OrderBestSeller> {
       child: ListView.builder(
           // scrollDirection: Axis.horizontal,
           shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
           itemCount: foodMenu.cat.length,
           itemBuilder: (context, i) {
             return renderFoodCategoryCard(foodMenu.cat[i]);
